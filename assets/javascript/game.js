c24c4bc2634680045cccd4e2fe1ed59b6d512c9a
Dana Cronin <cronin43224235@gmail.com>
2019-10-05 10:46:07 -0400

    pseudocoding and beginging setup for character objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,48 @@
 //==  GLOBAL VARIABLES  ==//
 //========================//
-
+var optionOne = {
+    health: ,
+    counterAttack: ,
+}
+var optionTwo = {
+    health: ,
+    counterAttack: ,
+}
+var optionThree = {
+    health: ,
+    counterAttack: ,
+}
+var optionFour = {
+    health: ,
+    counterAttack: ,
+}
 
 
 //===   FUNCTIONS   ===//
 //=====================//
 
+//when chooseing/assigning hero:
+//assign base attack?
+//var attack?
+//increases by base attack power with each click
+
 
 
 //==  CALLS / EVENTS  ==//
 //======================//
+
+
+//////////////// brainstorming?
+// dislay start screen
+//set character objects
+//select character
+//reasign non-clicked character classes
+//change display/output
+//buttons? attack/defend?
+//*selct oponent
+//*loop through attacks untill win/defeat
+//*select new oponent
+//*loop through attacks untill win/defeat
+//*last oponent
+//*loop through attacks untill win/defeat
+//win/lose /game-over screen
