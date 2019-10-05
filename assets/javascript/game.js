2b5f4b1986fdcb9993979a6105e0ad3e303d8c08
Dana Cronin <cronin43224235@gmail.com>
2019-10-05 12:57:54 -0400

    further structuring, naming functions or events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,54 @@
 //==  GLOBAL VARIABLES  ==//
 //========================//
-var optionOne = {
-    health: ,
-    counterAttack: ,
-}
-var optionTwo = {
-    health: ,
-    counterAttack: ,
-}
-var optionThree = {
-    health: ,
-    counterAttack: ,
-}
-var optionFour = {
-    health: ,
-    counterAttack: ,
-}
+var baseAttack = 7;
+
+// var opt1 = {
+//     health: ,
+//     counterAttack: ,
+// }
+// var opt2 = {
+//     health: ,
+//     counterAttack: ,
+// }
+// var opt3 = {
+//     health: ,
+//     counterAttack: ,
+// }
+// var opt4 = {
+//     health: ,
+//     counterAttack: ,
+// }
 
 
 //===   FUNCTIONS   ===//
 //=====================//
+function display(){
+
+}
+function user(){
+
+}
+function oponent(){
+
+}
 
 //when chooseing/assigning hero:
 //assign base attack?
 //var attack?
 //increases by base attack power with each click
 
-
-
 //==  CALLS / EVENTS  ==//
 //======================//
+$(".characters").on("click", function(){
+
+});
+$(".oponent").on("click", function(){
+
+});
 
 
-//////////////// brainstorming?
+/////    brainstorming?      //
+//===========================//
 // dislay start screen
 //set character objects
 //select character
